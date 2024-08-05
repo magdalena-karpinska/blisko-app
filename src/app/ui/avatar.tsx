850e1cb493ae6b7305a3ba9a6dd17481e4822ed5
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-05 23:42:49 +0200

    Adds avatarts components

---
+++ src/app/ui/avatar.tsx
@@ -0,0 +1,19 @@
+type AvatarProps = {
+  size?: "sm" | "md" | "lg";
+};
+
+export default function Avatar({ size = "sm" }: AvatarProps) {
+  const sizeClasses = {
+    sm: "w-16",
+    md: "w-24",
+    lg: "w-32",
+  };
+
+  return (
+    <div className="avatar pr-4">
+      <div className={`${sizeClasses[size]} rounded-full `}>
+        <img src="/magdalena.webp" alt="Magdalena" />
+      </div>
+    </div>
+  );
+}

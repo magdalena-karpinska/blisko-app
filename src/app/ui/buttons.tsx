574ad2c3867936bf77591bd703ec330973a76703
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-06 00:41:40 +0200

    Updates an icon button

--- src/app/ui/buttons.tsx
+++ src/app/ui/buttons.tsx
@@ -16,40 +16,23 @@ export function GhostButton({ children, className, ...rest }: ButtonProps) {
 
 export function IconButton({ children, className, ...rest }: ButtonProps) {
   return (
-    <div>
-      <button className="btn btn-circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </button>
-      <button className="btn btn-circle btn-outline">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </button>
-    </div>
+    <button className="btn btn-circle btn-outline">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M6 18L18 6M6 6l12 12"
+          transform="rotate(45 12 12)"
+        />
+      </svg>
+    </button>
   );
 }
 

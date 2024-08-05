9677eacdb2a21790d140f18d17f7832a5b180200
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-05 23:43:42 +0200

    Adds header and navbar components

---
+++ src/app/ui/header.tsx
@@ -0,0 +1,15 @@
+import Avatar from "./avatar";
+import { Heading3, PageHeading, Paragraph } from "./headings";
+import { NavBar } from "./nav-bar";
+
+export function Header() {
+  return (
+    <header className="lex flex-col items-left rounded-b-3xl overflow-hidden h-[18vh]">
+      <NavBar />
+      <div className="px-10 pt-5 max-w-[1100px] mx-auto w-full">
+        <Paragraph text="Welcome back," />
+        <PageHeading text="Magdalena" />
+      </div>
+    </header>
+  );
+}

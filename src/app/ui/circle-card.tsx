49d3fb1b7fafdd501526161174b1ff1e12b1aba0
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-05 23:41:44 +0200

    Adds circles components

---
+++ src/app/ui/circle-card.tsx
@@ -0,0 +1,37 @@
+type CustomColors =
+  | "primary"
+  | "base-100"
+  | "white"
+  | "darkOrange"
+  | "orange"
+  | "lightOrange"
+  | "lightBlue"
+  | "blue"
+  | "gray";
+
+type CardProps = {
+  text: string;
+  bgColor: CustomColors;
+};
+
+export function CircleCard({ text, bgColor }: CardProps) {
+  const bgColorClass = {
+    primary: "bg-[#84A9CD]",
+    "base-100": "bg-[#FFF9E6]",
+    white: "bg-[#FFF]",
+    darkOrange: "bg-[#FC9F66]",
+    orange: "bg-[#FAC357]",
+    lightOrange: "bg-[#FAE39C]",
+    lightBlue: "bg-[#B8E0E3]",
+    blue: "bg-[#97C5D8]",
+    gray: "bg-[#D1D1D6]",
+  }[bgColor];
+
+  return (
+    <div className={`card w-full ${bgColorClass}`}>
+      <div className="card-body">
+        <h2 className="card-title">{text}</h2>
+      </div>
+    </div>
+  );
+}

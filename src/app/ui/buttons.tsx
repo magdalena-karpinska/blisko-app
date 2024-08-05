7eb7dc8e847e6efa04f55629b5834e7d318bb773
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-05 23:41:05 +0200

    Adds buttonss components

---
+++ src/app/ui/buttons.tsx
@@ -0,0 +1,62 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export function PrimaryButton({ children, className, ...rest }: ButtonProps) {
+  return <button className="btn btn-primary">{children}</button>;
+}
+
+export function SecondaryButton({ children, className, ...rest }: ButtonProps) {
+  return <button className="btn btn-secondary">{children}</button>;
+}
+
+export function GhostButton({ children, className, ...rest }: ButtonProps) {
+  return <button className="btn btn-ghost">{children}</button>;
+}
+
+export function IconButton({ children, className, ...rest }: ButtonProps) {
+  return (
+    <div>
+      <button className="btn btn-circle">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
+      <button className="btn btn-circle btn-outline">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+}
+
+export function DisabledButton({ children, className, ...rest }: ButtonProps) {
+  return (
+    <button className="btn" disabled={true}>
+      {children}
+    </button>
+  );
+}

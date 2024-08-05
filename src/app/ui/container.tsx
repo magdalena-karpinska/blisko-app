61e2c0776a5e198b6485a796caa9cac18aa6399e
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-05 23:45:07 +0200

    Adds container component

---
+++ src/app/ui/container.tsx
@@ -0,0 +1,9 @@
+export default function Container({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className="max-w-[1100px] w-full mx-auto px-4 md:px-8 lg:px-16 flex flex-col flex-1 justify-end pb-20">
+      {children}
+    </div>
+  );
+}

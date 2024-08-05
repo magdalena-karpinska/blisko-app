00c0c53c9b15a61248030ec22262adcd58e91337
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-05 23:47:48 +0200

    Creates / route with the Dashboard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <h1>Hello Blisko App!</h1>;
+import { CircleCard } from "./ui/circle-card";
+
+export default function Dashboard() {
+  return (
+    <main className="w-full flex flex-col items-center space-y-4 my-4">
+      <CircleCard text="All" bgColor="blue" />
+      <CircleCard text="Acquaintances" bgColor="lightBlue" />
+      <CircleCard text="Friends" bgColor="lightOrange" />
+      <CircleCard text="Family" bgColor="orange" />
+    </main>
+  );
 }

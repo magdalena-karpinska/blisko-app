d8d802403966dd765ca75cec86c6f5d4f1970ac0
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-06 00:23:58 +0200

    Adds divider components and improves styling

--- src/app/design-system/page.tsx
+++ src/app/design-system/page.tsx
@@ -9,36 +9,60 @@ import {
 import { CircleCard } from "../ui/circle-card";
 import { ConnectionCard } from "../ui/connection-card";
 import SearchInput from "../ui/input";
+import Divider from "../ui/divider";
 
 export default function designSystem() {
   return (
     <main className="w-full flex flex-col items-center space-y-4 my-8">
       <Heading1 text="Design System" />
-      <Heading3 text="Buttons" />
-      <Heading3 text="Primary button" />
-      <Paragraph text="Usage: Submit, SignUp, Send Message, Create Circle" />
-      <PrimaryButton>Submit</PrimaryButton>
-      <Heading3 text="Secondary button" />
-      <Paragraph text="Usage: Back, Cancel, Edit Profile" />
-      <SecondaryButton>Cancel</SecondaryButton>
-      <Heading3 text="Ghost button" />
-      <Paragraph text="Usage: Skip, Learn more" />
-      <GhostButton>Skip</GhostButton>
-      <Heading3 text="Icon button" />
-      <Paragraph text="Usage: Add a connection" />
-      <IconButton>Skip</IconButton>
-      <Heading3 text="Disabled button" />
-      <Paragraph text="Usage: for conditional rendering" />
-      <DisabledButton>Submit</DisabledButton>
-      <Heading3 text="Circle Card" />
-      <Paragraph text="Usage: for connection circles" />
-      <CircleCard text="Family" bgColor={"blue"} />
-      <Heading3 text="Contact Card" />
-      <Paragraph text="Usage: for connections" />
-      <ConnectionCard text="Magdalena Karpinska" />
-      <Heading3 text="Input" />
-      <Paragraph text="Usage: Search input field" />
-      <SearchInput />
+      <div className="py-8 w-full flex flex-col items-center space-y-4">
+        <Divider text="Inputs" />
+        <Heading3 text="Search input" />
+        <Paragraph text="Usage: Search input field" />
+        <SearchInput />
+      </div>
+      <div className="py-8 w-full flex flex-col items-center space-y-4">
+        <Divider text="Buttons" />
+        <Heading3 text="Primary button" />
+        <Paragraph text="Usage: Submit, SignUp, Send Message, Create Circle" />
+        <PrimaryButton>Submit</PrimaryButton>
+      </div>
+      <div className="py-8 w-full flex flex-col items-center space-y-4">
+        <Heading3 text="Secondary button" />
+        <Paragraph text="Usage: Back, Cancel, Edit Profile" />
+        <SecondaryButton>Cancel</SecondaryButton>
+      </div>
+
+      <div className="py-8 w-full flex flex-col items-center space-y-4">
+        <Heading3 text="Ghost button" />
+        <Paragraph text="Usage: Skip, Learn more" />
+        <GhostButton>Skip</GhostButton>
+      </div>
+
+      <div className="py-8 w-full flex flex-col items-center space-y-4">
+        <Heading3 text="Icon button" />
+        <Paragraph text="Usage: Add a connection" />
+        <IconButton>Skip</IconButton>
+      </div>
+
+      <div className="py-8 w-full flex flex-col items-center space-y-4">
+        <Heading3 text="Disabled button" />
+        <Paragraph text="Usage: for conditional rendering" />
+        <DisabledButton>Submit</DisabledButton>
+      </div>
+
+      <div className="py-8 w-full flex flex-col items-center space-y-4">
+        <Divider text="Cards" />
+        <Heading3 text="Circle Card" />
+        <Paragraph text="Usage: for connection circles" />
+        <CircleCard text="Family" bgColor={"blue"} />
+      </div>
+
+      <div className="py-8 w-full flex flex-col items-center space-y-4">
+        <Heading3 text="Contact Card" />
+        <Paragraph text="Usage: for connections" />
+        <ConnectionCard text="Magdalena Karpinska" />
+      </div>
     </main>
   );
 }

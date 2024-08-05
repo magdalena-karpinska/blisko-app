373d91caa5f9f7a77d488d44cba61c490f6c6b1c
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-05 23:44:27 +0200

    Adds footer component

---
+++ src/app/ui/footer.tsx
@@ -0,0 +1,12 @@
+import Avatar from "./avatar";
+
+export default function Footer() {
+  return (
+    <footer className="footer footer-center p-4 bg-secondary text-primary-content relative rounded-t-[20px]">
+      <div className="absolute left-1/2 transform -translate-x-1/2 -top-16">
+        <Avatar size="lg" />
+      </div>
+      <aside className="pt-16"></aside>
+    </footer>
+  );
+}

6dcdcfd6961eb1ec65ccdaeebf6a0cd9d0c38f15
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-05 14:19:21 +0200

    Adds font

--- src/app/design-system/page.tsx
+++ src/app/design-system/page.tsx
@@ -5,25 +5,29 @@ import {
   IconButton,
   DisabledButton,
 } from "../ui/button";
+import { typography } from "../ui/typography";
 
 export default function designSystem() {
   return (
     <div>
-      <h2>Buttons</h2>
-      <h3>Primary button</h3>
-      <p>Usage: Submit, SignUp, Send Message, Create Circle</p>
+      <h1 className={typography.h1}>Design System</h1>
+      <h2 className={typography.h2}>Buttons</h2>
+      <h3 className={typography.h3}>Primary button</h3>
+      <p className={typography.body}>
+        Usage: Submit, SignUp, Send Message, Create Circle
+      </p>
       <PrimaryButton>Submit</PrimaryButton>
-      <h3>Secondary button</h3>
-      <p>Usage: Back, Cancel, Edit Profile</p>
+      <h3 className={typography.h3}>Secondary button</h3>
+      <p className={typography.body}>Usage: Back, Cancel, Edit Profile</p>
       <SecondaryButton>Cancel</SecondaryButton>
-      <h3>Ghost button</h3>
-      <p>Usage: Skip, Learn more</p>
+      <h3 className={typography.h3}>Ghost button</h3>
+      <p className={typography.body}>Usage: Skip, Learn more</p>
       <GhostButton>Skip</GhostButton>
-      <h3>Icon button</h3>
-      <p>Usage: Add a connection</p>
+      <h3 className={typography.h3}>Icon button</h3>
+      <p className={typography.body}>Usage: Add a connection</p>
       <IconButton>Skip</IconButton>
-      <h3>Disabled button</h3>
-      <p>Usage: for conditional rendering</p>
+      <h3 className={typography.h3}>Disabled button</h3>
+      <p className={typography.body}>Usage: for conditional rendering</p>
       <DisabledButton>Submit</DisabledButton>
     </div>
   );
---
+++ src/app/ui/typography.ts
@@ -0,0 +1,9 @@
+import { lexend } from "./fonts";
+
+// Typography
+export const typography = {
+  h1: `var(--font-lexend) text-4xl font-black antialiased`, // 900 weight
+  h2: `var(--font-lexend) text-3xl font-bold antialiased`, // 700 weight
+  h3: `var(--font-lexend) text-2xl font-medium antialiased`, // 500 weight
+  body: `var(--font-lexend) text-base font-normal antialiased`, // 400 weight
+};

21ada63017de3c64fa89ca9ab1b0d20e65019922
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-10 15:05:13 +0200

    Adjusts styling of a container

--- src/app/ui/container.tsx
+++ src/app/ui/container.tsx
@@ -2,7 +2,7 @@ export function Container({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="max-w-[1100px] w-full mx-auto px-4 md:px-8 lg:px-16 flex flex-col flex-1 justify-end pb-20">
+    <div className="max-w-[1100px] w-full mx-auto px-4 md:px-8 lg:px-16 flex flex-col flex-1 pb-20">
       {children}
     </div>
   );

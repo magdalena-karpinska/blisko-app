4f5b6af669b70577d9992c21b280128dc2fa26b8
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-11 17:38:45 +0200

    Updates container component to be scrollable

--- src/app/ui/container.tsx
+++ src/app/ui/container.tsx
@@ -2,8 +2,8 @@ export function Container({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="max-w-[1100px] w-full mx-auto px-4 md:px-8 lg:px-16 flex flex-col flex-1 pb-20">
-      <main className="w-full flex flex-col items-center space-y-4 my-8">
+    <div className="flex-grow overflow-y-auto">
+      <main className="w-full flex flex-col items-center space-y-4 my-8 pb-16 px-4 md:px-8 lg:px-16">
         {children}
       </main>
     </div>

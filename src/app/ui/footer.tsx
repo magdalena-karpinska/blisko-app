91b28999662c57408fdc59c920aee1cd6dc7fc0c
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-11 13:34:08 +0200

    Adds icons

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -1,12 +1,22 @@
+import Link from "next/link";
+
+import { BsEnvelopeHeart } from "react-icons/bs";
+import { IoCalendarNumberOutline } from "react-icons/io5";
+
 import { Avatar } from ".";
 
-export default function Footer() {
+export function Footer() {
   return (
-    <footer className="footer footer-center p-4 bg-secondary text-primary-content relative rounded-t-[20px] max-w-[1100px] w-full">
+    <footer className="footer footer-center p-4 h-24 bg-secondary text-primary-content relative rounded-t-[20px] max-w-[1100px] w-full">
+      <div className="flex justify-between items-center w-full px-12">
+        <Link href="/messages">
+          <BsEnvelopeHeart className="text-3xl" />
+        </Link>
+        <IoCalendarNumberOutline className="text-3xl" />
+      </div>
       <div className="absolute left-1/2 transform -translate-x-1/2 -top-16">
         <Avatar size="lg" />
       </div>
-      <aside className="pt-16"></aside>
     </footer>
   );
 }

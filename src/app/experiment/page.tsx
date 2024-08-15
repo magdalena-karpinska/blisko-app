5ed9b2ef63a668fed9d93a0aa02c5648e3ad260e
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-15 08:44:57 +0200

    Creates a list of names

--- src/app/experiment/page.tsx
+++ src/app/experiment/page.tsx
@@ -1,3 +1,12 @@
 export default function Page() {
-  return <h1>Experiment</h1>;
+  return (
+    <>
+      <h1>Experiment</h1>
+      <div>
+        <p>Magdalena</p>
+        <p>Anna</p>
+        <p>Ula</p>
+      </div>
+    </>
+  );
 }

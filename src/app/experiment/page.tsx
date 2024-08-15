41f9858c35b1ae13e43b34b27deac59aa22d0e3c
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-15 08:46:46 +0200

    maps through names array

--- src/app/experiment/page.tsx
+++ src/app/experiment/page.tsx
@@ -1,11 +1,12 @@
 export default function Page() {
+  const connectionNames = ["Magdalena", "Anna", "Ula"];
   return (
     <>
       <h1>Experiment</h1>
       <div>
-        <p>Magdalena</p>
-        <p>Anna</p>
-        <p>Ula</p>
+        {connectionNames.map((name) => (
+          <div>{name}</div>
+        ))}
       </div>
     </>
   );
